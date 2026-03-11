44fec62ad92dbb7eb555a9a3f2be42fdf537d3b1
Arifjawad18 <arif.jawad18@gmail.com>
2026-03-12 01:35:08 +0600

    done all

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { Suspense } from "react";
+import "./App.css";
+import Customers from "./components/Customers/Customers";
+
+const fetchCustomers = async () => {
+  const res = await fetch("/customers.json");
+  return res.json();
+};
+function App() {
+  const customersPromise = fetchCustomers();
+
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-md border-b border-base-200 px-4">
+        <div className="flex-1 gap-2">
+          <a className="text-xl font-bold tracking-tight cursor-pointer">
+            CS — Ticket System
+          </a>
+        </div>
+
+        <div className="navbar-end gap-3">
+          <div className="lg:flex">
+            <ul className="menu menu-horizontal px-1 gap-1">
+              <li>
+                <a className="rounded-lg active:bg-primary">Dashboard</a>
+              </li>
+              <li>
+                <a className="rounded-lg">Tickets</a>
+              </li>
+              <li>
+                <a className="rounded-lg">Customers</a>
+              </li>
+              <li>
+                <a className="rounded-lg">Reports</a>
+              </li>
+            </ul>
+          </div>
+
+          <button className="btn btn-primary btn-sm md:btn-md shadow-lg shadow-primary/20">
+            <span className="hidden md:inline">+ New Ticket</span>
+            <span className="md:hidden">+ New Ticket</span>
+          </button>
+        </div>
+      </div>
+      <Suspense
+        fallback={<span className="loading loading-ring loading-xs"></span>}
+      >
+        <Customers customersPromise={customersPromise} />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
